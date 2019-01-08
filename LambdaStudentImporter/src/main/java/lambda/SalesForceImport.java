bc47de87ef0d17c0a418bbc80310e436f155a77f
wavis421 <wendy.avis@jointheleague.org>
2019-01-08 13:05:23 -0800

    Initial commit of AWS Lambda Student Importer.

---
+++ LambdaStudentImporter/src/main/java/lambda/SalesForceImport.java
@@ -0,0 +1,98 @@
+package lambda;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.sforce.soap.enterprise.Connector;
+import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+
+import controller.Pike13Connect;
+import controller.Pike13SalesforceImport;
+import controller.SalesForceImportEngine;
+import model.LocationLookup;
+import model.LogDataModel;
+import model.MySqlDatabase;
+import model.MySqlDbLogging;
+import model.StudentNameModel;
+
+public class SalesForceImport {
+	// Import -30 to +45 days
+	private static final int DATE_RANGE_PAST_IN_DAYS = 21;
+	private static final int DATE_RANGE_FUTURE_IN_DAYS = 45;
+	private static final int DATE_RANGE_ENROLL_STATS_DAYS = 20;
+
+	MySqlDatabase sqlDb;
+	String startDate, endDate;
+
+	public String myHandler(Object input, Context context) {
+
+		// Set start/end date to -30/+90 days
+		DateTime t = new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles"));
+		String today = t.toString("yyyy-MM-dd HH:mm:ss");
+		startDate = t.minusDays(DATE_RANGE_PAST_IN_DAYS).toString("yyyy-MM-dd");
+		endDate = t.plusDays(DATE_RANGE_FUTURE_IN_DAYS).toString("yyyy-MM-dd");
+
+		LambdaLogger logger = context.getLogger();
+		logger.log(today + " " + input + "\n");
+
+		// Connect to database
+		sqlDb = new MySqlDatabase(System.getenv("PASSWORD"), MySqlDatabase.STUDENT_IMPORT_NO_SSH);
+		if (!sqlDb.connectDatabase()) {
+			logger.log("Failure connecting to mySql database!");
+			return ("Start: " + today + ", End: " + (new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles"))
+					.toString("yyyy-MM-dd HH:mm:ss")));
+		}
+
+		new MySqlDbLogging(sqlDb);
+		MySqlDbLogging.insertLogData(LogDataModel.STARTING_SALES_FORCE_IMPORT, new StudentNameModel("", "", false), 0,
+				" from " + startDate + " to " + endDate + " ***");
+
+		// Connect to Pike13
+		Pike13Connect pike13Conn = new Pike13Connect(System.getenv("PIKE13_KEY"));
+		Pike13SalesforceImport pike13Api = new Pike13SalesforceImport(pike13Conn);
+
+		// Connect to SalesForce
+		EnterpriseConnection salesForceApi;
+		ConnectorConfig config = new ConnectorConfig();
+		config.setUsername(System.getenv("SALESFORCE_USER"));
+		config.setPassword(System.getenv("SALESFORCE_KEY"));
+		config.setTraceMessage(false);
+
+		try {
+			salesForceApi = Connector.newConnection(config);
+
+		} catch (ConnectionException e1) {
+			lambdaFunctionEnd(LogDataModel.SALES_FORCE_CONNECTION_ERROR, e1.getMessage());
+			return ("Start: " + today + ", End: " + (new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles"))
+					.toString("yyyy-MM-dd HH:mm:ss")));
+		}
+
+		// Perform the update to SalesForce
+		SalesForceImportEngine importer = new SalesForceImportEngine(sqlDb, pike13Api, salesForceApi);
+		LocationLookup.setLocationData(sqlDb.getLocationList());
+		importer.updateSalesForce(today, startDate, endDate, DATE_RANGE_ENROLL_STATS_DAYS);
+
+		// Clean up and exit
+		lambdaFunctionEnd(-1, null); // -1 indicates no error
+		return ("Start: " + today + ", End: "
+				+ (new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles")).toString("yyyy-MM-dd HH:mm:ss")));
+	}
+
+	private void lambdaFunctionEnd(int errorCode, String errorMessage) {
+		if (errorCode == -1) {
+			// Success
+			MySqlDbLogging.insertLogData(LogDataModel.SALES_FORCE_IMPORT_COMPLETE, new StudentNameModel("", "", false),
+					0, " from " + startDate + " to " + endDate + " ***");
+		} else {
+			// Failure
+			MySqlDbLogging.insertLogData(errorCode, new StudentNameModel("", "", false), 0, ": " + errorMessage);
+			MySqlDbLogging.insertLogData(LogDataModel.SALES_FORCE_IMPORT_ABORTED, new StudentNameModel("", "", false),
+					0, " from " + startDate + " to " + endDate + " ***");
+		}
+		sqlDb.disconnectDatabase();
+	}
+}
---
+++ LambdaStudentImporter/src/main/java/lambda/StudentImport.java
@@ -0,0 +1,79 @@
+package lambda;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import controller.GithubApi;
+import controller.Pike13Connect;
+import controller.Pike13DbImport;
+import controller.StudentImportEngine;
+import model.LocationLookup;
+import model.LogDataModel;
+import model.MySqlDatabase;
+import model.MySqlDbImports;
+import model.MySqlDbLogging;
+import model.StudentNameModel;
+
+/**
+ * The Student Data Import class is the controller for the nightly import of
+ * data from Pike13 to The League AWS Tracker Database. Imported data includes
+ * Student data, Attendance data and class/course Schedule data.
+ * 
+ * @author wavis
+ *
+ */
+
+public class StudentImport {
+
+	private static final int ATTEND_NUM_DAYS_IN_PAST = 21;
+	private static final int ATTEND_NUM_DAYS_IN_FUTURE = 120;
+	private MySqlDatabase sqlDb;
+
+	public String myHandler(Object input, Context context) {
+		// Import data starting 7 days ago
+		DateTime today = new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles"));
+		String startDateString = today.minusDays(ATTEND_NUM_DAYS_IN_PAST).toString().substring(0, 10);
+		String courseEndDate = today.plusDays(ATTEND_NUM_DAYS_IN_FUTURE).toString().substring(0, 10);
+
+		LambdaLogger logger = context.getLogger();
+		logger.log(today.toString("yyyy-MM-dd HH:mm:ss") + " " + input + "\n");
+
+		// Connect to database
+		sqlDb = new MySqlDatabase(System.getenv("PASSWORD"), MySqlDatabase.STUDENT_IMPORT_NO_SSH);
+		if (sqlDb.connectDatabase()) {
+			new MySqlDbLogging(sqlDb);
+			MySqlDbLogging.insertLogData(LogDataModel.STARTING_TRACKER_IMPORT, new StudentNameModel("", "", false), 0,
+					" for " + today.toString("yyyy-MM-dd") + " ***");
+
+			MySqlDbImports sqlImportDb = new MySqlDbImports(sqlDb);
+			StudentImportEngine importer = new StudentImportEngine(sqlImportDb);
+			LocationLookup.setLocationData(sqlDb.getLocationList());
+
+			// Remove log data older than 7 days
+			importer.removeOldLogData(7);
+
+			// Connect to Pike13 and import data
+			Pike13Connect pike13Conn = new Pike13Connect(System.getenv("PIKE13_KEY"));
+			Pike13DbImport pike13Api = new Pike13DbImport(sqlImportDb, pike13Conn);
+			importer.importStudentsFromPike13(pike13Api);
+			importer.importAttendanceFromPike13(startDateString, pike13Api);
+			importer.importScheduleFromPike13(pike13Api);
+			importer.importCoursesFromPike13(pike13Api);
+			importer.importCourseAttendanceFromPike13(startDateString, courseEndDate, pike13Api);
+
+			// Connect to Github and import data
+			GithubApi githubApi = new GithubApi(sqlImportDb, System.getenv("GITHUB_KEY"));
+			importer.importGithubComments(startDateString, githubApi);
+
+			MySqlDbLogging.insertLogData(LogDataModel.TRACKER_IMPORT_COMPLETE, new StudentNameModel("", "", false), 0,
+					" for " + today.toString("yyyy-MM-dd") + " ***");
+			sqlDb.disconnectDatabase();
+		}
+
+		return ("Start: " + today.toString("yyyy-MM-dd HH:mm:ss") + ", End: "
+				+ (new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles")).toString("yyyy-MM-dd HH:mm:ss")));
+	}
+}
